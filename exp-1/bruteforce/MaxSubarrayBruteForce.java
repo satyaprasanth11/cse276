ee81eda4c4cbac868d00379efbd2037a0c29e481
satyaprasanth11 <sathyak23389@gmail.com>
2026-07-14 19:17:16 +0530

    updated bruteforce

--- exp-1/bruteforce/MaxSubarrayBruteForce.java
+++ exp-1/bruteforce/MaxSubarrayBruteForce.java
@@ -1,40 +1,25 @@
 import java.util.Scanner;
-
 public class MaxSubarrayBruteForce {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter number of elements: ");
         int n = sc.nextInt();
-
         int[] arr = new int[n];
-
         System.out.println("Enter array elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
         int maxSum = Integer.MIN_VALUE;
-
-        // Brute Force
         for (int i = 0; i < n; i++) {
-
             int currentSum = 0;
-
             for (int j = i; j < n; j++) {
-
                 currentSum += arr[j];
-
                 if (currentSum > maxSum) {
                     maxSum = currentSum;
                 }
             }
         }
-
         System.out.println("Maximum Subarray Sum = " + maxSum);
-
         sc.close();
     }
 }
