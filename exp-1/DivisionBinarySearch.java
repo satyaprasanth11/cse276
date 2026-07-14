f3fc9be11c7c7cd04bec2d380d46be5e5492a3fe
satyaprasanth11 <sathyak23389@gmail.com>
2026-07-14 19:01:00 +0530

    updated code

--- exp-1/DivisionBinarySearch.java
+++ exp-1/DivisionBinarySearch.java
@@ -1,22 +1,15 @@
 import java.util.Scanner;
-
 public class DivisionBinarySearch {
-
     public static int divide(int dividend, int divisor) {
-
         if (divisor == 0) {
             System.out.println("Division by zero is not allowed.");
             return 0;
         }
-
         int low = 0;
         int high = dividend;
         int answer = 0;
-
         while (low <= high) {
-
             int mid = (low + high) / 2;
-
             if (mid * divisor <= dividend) {
                 answer = mid;
                 low = mid + 1;
@@ -24,27 +17,19 @@ public class DivisionBinarySearch {
                 high = mid - 1;
             }
         }
-
         return answer;
     }
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter Dividend: ");
         int dividend = sc.nextInt();
-
         System.out.print("Enter Divisor: ");
         int divisor = sc.nextInt();
-
         if (dividend < 0 || divisor <= 0) {
             System.out.println("Enter positive numbers only.");
             return;
         }
-
         int quotient = divide(dividend, divisor);
-
         System.out.println("Quotient = " + quotient);
     }
 }
